c50341e8f639d7142871ff9080467ff49500fe46
Christian Digiorno <cdigiorno@devartis.com>
2018-07-30 12:25:27 -0300

    Cambio forma de usar base.render

--- ckanext/seriestiempoarlanding/seriestiempoarcontroller.py
+++ ckanext/seriestiempoarlanding/seriestiempoarcontroller.py
@@ -6,4 +6,4 @@ logger = logging.getLogger(__name__)
 class SeriesTiempoArController(BaseController):
 
     def test(self):
-        return base.render('config/config_01_title.html')
+        return ckan.lib.base.render('config/config_01_title.html')
